740a287f7203730ec69358ffc24081740c1126bf
GBachLe <131783349+GBachLe@users.noreply.github.com>
2023-05-06 15:20:01 +1000

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,27 @@
+package bigO;
+
+import java.util.Iterator;
+
+public class fibonacci {
+public static void main(String[] args) {
+	int num = 6;
+	System.out.println(findFibonacci(num));
+}
+
+
+public static int findFibonacci (int num) {
+	if ( num == 0 || num == 1 ) {
+		return num;
+	}
+	
+	else {
+		int[] fibo = new int[num+1];
+		fibo[0] = 1;
+		fibo[1] = 1;
+		for (int i = 2; i < fibo.length; i++) {
+			fibo[i] = fibo[i-1] + fibo[i-2];
+		}
+		return fibo[num];
+	}
+}
+}
